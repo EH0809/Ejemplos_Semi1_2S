625db9c9e9de133bbf095da2c6fe6cc039d15f33
Edgar Herrera <2807941500101@ingenieria.usac.edu.gt>
2022-09-02 23:19:28 -0600

    Ejemplo db

--- Servidor/index.js
+++ Servidor/index.js
@@ -4,18 +4,30 @@ var app = express();
 
 const cors = require('cors');
 
+
 var corsOptions = { origin: true, optionsSuccessStatus: 200 };
 app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 var AWS = require('aws-sdk');
-
+//Nuevo 
+const mysql = require('mysql');  //<- para importar la base de datos 
+const aws_keys = require('./creds_template'); // <-- se agrega la clase en donde estan las credenciales 
 
 var port = 9000;
 app.listen(port);
 console.log("Escuchando en el puerto", port)
 
+
+// se manda a llamar las credenciales de Mysql 
+const db_credentials = require('./db_creds'); //<-- Se importa las credenciales de la base de datos 
+var conn = mysql.createPool(db_credentials); // <- Se crea un pool para realizar la conexion a la base de datos 
+
+
+// Se instancian todos los objetos de aws 
+const s3 = new AWS.S3(aws_keys.s3);
+const ddb = new AWS.DynamoDB(aws_keys.dynamodb);
 
 /*
 app.get('/', function (req, res ) {
@@ -23,6 +35,8 @@ app.get('/', function (req, res ) {
 });
 */
 
+
+//---------------------------------Ejemplo S3----------------------------------
 app.post('/subirfoto', function (req, res){
 
     var id = req.body.id;
@@ -86,7 +100,77 @@ app.post('/obtenerfoto', function (req, res) {
 
 });
 
+//---------------------------------Ejemplo DB ------------------------------------
 
 
+///DYNAMO 
+//subir foto y guardar en dynamo
+app.post('/saveImageInfoDDB', (req, res) => {
+    let body = req.body;
+
+    let name = body.name;
+    let base64String = body.base64;
+    let extension = body.extension;
+
+    //Decodificar imagen
+    let encodedImage = base64String;
+    let decodedImage = Buffer.from(encodedImage, 'base64');
+    let filename = `${name}.${extension}`; 
+
+    //Parámetros para S3
+    let bucketname = 'pruebadynamo'; //<--- Nombre del bucket 
+    let folder = 'fotos/';
+    let filepath = `${folder}${filename}`;
+    var uploadParamsS3 = {
+        Bucket: bucketname,
+        Key: filepath,
+        Body: decodedImage,
+        ACL: 'public-read', // ACL -> LE APLICA LA POLITICA AL OBJETO QUE SE ESTA GUARDANDO
+    };
+
+    s3.upload(uploadParamsS3, function sync(err, data) {
+        if (err) {
+            console.log('Error uploading file:', err);
+            res.send({ 'message': 's3 failed' })
+        } else {
+            console.log('Upload success at:', data.Location);
+            ddb.putItem({
+                TableName: "Prueba_1", // el nombre de la tabla de dynamoDB 
+                Item: {
+                    "id": { S: "3" },
+                    "name": { S: name },
+                    "location": { S: data.Location }
+                }
+            }, function (err, data) {
+                if (err) {
+                    console.log('Error saving data:', err);
+                    res.send({ 'message': 'ddb failed' });
+                } else {
+                    console.log('Save success:', data);
+                    res.send({ 'message': 'ddb success' });
+                }
+            });
+        }
+    });
+})
+
+
+/******************************RDS *************/
+//obtener datos de la BD
+app.get("/getdata", async (req, res) => {
+    conn.query(`SELECT * FROM ejemplo`, function (err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+//insertar datos
+app.post("/insertdata", async (req, res) => {
+    let body = req.body;
+    conn.query('INSERT INTO ejemplo VALUES(?,?)', [body.id, body.nombre], function (err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 
 			
